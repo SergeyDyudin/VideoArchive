18fa662dbb27693a4e302275d2f2121160915b34
Sergey <sergeydyudin@yandex.ru>
2020-03-20 17:35:21 +0300

    Add pdf_to_text.py

--- OCR/pdf_to_text.py
+++ OCR/pdf_to_text.py
@@ -1,9 +1,9 @@
-""" pip3 install PIL
+""" pip3 install Pillow
     pip3 install pytesseract
     pip3 install pdf2image
     sudo apt-get install tesseract-ocr
 
-    Прописать в PATH popller и tesseract
+    Скачать, установить и прописать в PATH popller и tesseract
 """
 
 from PIL import Image
@@ -13,7 +13,7 @@ from pdf2image import convert_from_path
 PDF_file = r"C:\Install\test.pdf"  # исходник PDF
 
 
-""" Преобразование PDF в изображения
+""" Преобразование PDF в JPEG
 """
 
 pages = convert_from_path(PDF_file, 500)  # Хранить все страницы PDF в переменной
@@ -25,7 +25,7 @@ for page in pages:
     page.save(filename, 'JPEG')  # Сохранить изображение страницы в системе
     image_counter = image_counter + 1
 
-""" Распознавание текста по изображениям с помощью OCR
+""" Распознавание текста по изображениям с помощью Tesseract OCR
 """
 
 filelimit = image_counter - 1  # Переменная, чтобы получить общее количество страниц
