db21d6f745bc379a45b460d08bd3d483f671c6c7
Sergey Dyudin <sergeydyudin@yandex.ru>
2020-04-12 21:06:32 +0300

    Add Selenium test

---
+++ Scripts/test.py
@@ -0,0 +1,56 @@
+import selenium
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import openpyxl
+from Scripts.kinopoisk_parser import KinopoiskParser
+import time
+
+url = 'https://www.kinopoisk.ru/'
+browser = webdriver.Firefox()  # firefox_profile=r'C:\Users\video\AppData\Roaming\Mozilla\Firefox\Profiles\h3qugs8n.Kinopisk')
+browser.get(url)
+# search_form = browser.find_element_by_name('kp_query')
+
+wb = openpyxl.load_workbook(filename='C:/install/Films.xlsx')
+ws = wb.active
+for row in ws.iter_rows(min_row=2, max_row=ws.max_row):
+    if row[1].value == 'Сериал': continue  # пока не трогаем сериалы
+    if not row[4].value: continue  # пока не трогаем первую плохо заполеннную часть таблицы
+    name_film = row[0].value.lower()
+    year_film = str(row[4].value)
+    search_form = browser.find_element_by_name('kp_query')
+    search_form.clear()
+    search_form.send_keys(name_film)
+    search_form.submit()
+    time.sleep(6)
+    WebDriverWait(browser,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'name')))
+    WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.PARTIAL_LINK_TEXT, name_film.capitalize())))
+    # get = browser.page_source
+    # soup = BeautifulSoup(get, features="html.parser")
+    # results = soup.find_all('p', {'class': 'name'})
+    results = browser.find_elements_by_class_name('name')
+    for result in results:
+        try:
+            year = result.find_element_by_class_name('year').text
+        except:
+            break
+        if '-' in year:
+            continue
+        """ else:
+            year = int(year)"""
+        name = result.find_element_by_tag_name('a').text
+        name = name.replace(':', '.')
+        if (name_film == name.lower()) and (year_film == year):
+            result.find_element_by_tag_name('a').click()
+            break
+    try:
+        WebDriverWait(browser,10).until(EC.presence_of_element_located((By.CLASS_NAME,'info')))
+        WebDriverWait(browser,10).until(EC.presence_of_element_located((By.ID,'actorList')))
+        get = browser.page_source
+        res = KinopoiskParser.get_info(get)
+        print(res)
+    except selenium.common.exceptions.TimeoutException:
+        print('Что-то пошло не так с этим фильмом')
+browser.close()
