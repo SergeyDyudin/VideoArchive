7c77588e2e18b257c39b9dca2f1338c0f485fe73
Sergey <sergeydyudin@yandex.ru>
2020-06-18 17:33:07 +0300

    Writing db.py

--- db.py
+++ db.py
@@ -61,7 +61,7 @@ class DataBase:
             result['name'] = ws.cell(row=num_str, column=self._find_column(ws, 'Name')).value
             result['type'] = ws.cell(row=num_str, column=self._find_column(ws, 'Type')).value
             result['season'] = ws.cell(row=num_str, column=self._find_column(ws, 'Season')).value
-            result['year'] = ws.cell(row=num_str, column=self._find_column(ws, 'Year')).value
+            result['year'] = str(ws.cell(row=num_str, column=self._find_column(ws, 'Year')).value)
             result['genre'] = ws.cell(row=num_str, column=self._find_column(ws, 'Genre')).value
             result['id_kinopoisk'] = ws.cell(row=num_str, column=self._find_column(ws, 'Kinopoisk ID')).value
             result['imdb'] = ws.cell(row=num_str, column=self._find_column(ws, 'IMDB')).value
@@ -70,6 +70,7 @@ class DataBase:
             result['time'] = ws.cell(row=num_str, column=self._find_column(ws, 'Time')).value
             result['director'] = ws.cell(row=num_str, column=self._find_column(ws, 'Director')).value
             result['actors'] = ws.cell(row=num_str, column=self._find_column(ws, 'Actors')).value
+            if result['season']: result['season'] = result['season'].lower()
         finally:
             wb.close()
         return result
@@ -80,18 +81,51 @@ class DataBase:
         :param request: запрос
         :return:
         """
-        data = self.get_data(8)
-        request = 'INSERT INTO {table} ({field}) VALUES (%s) ON CONFLICT ({field}) DO UPDATE SET {field}=Excluded.{field} Returning id;'
+        data = self.get_data(7)
+        request = """
+                    INSERT INTO {table} ({field}) 
+                    VALUES (LOWER(%s)) 
+                    ON CONFLICT ({field}) DO UPDATE SET {field}=LOWER(Excluded.{field}) 
+                    Returning id;
+                    """
         try:
+            # Заполнение таблицы years и получение id_years
             # self.cur.execute('INSERT INTO years (year) VALUES (%(year)s);', data)
             self.cur.execute(sql.SQL(request).format(table=sql.Identifier('years'),
                                                      field=sql.Identifier('year')), (data['year'],))
+            id_years = self.cur.fetchone()[0]
+            # Заполнение таблицы genres и получение id_genres
+            self.cur.execute(sql.SQL(request).format(table=sql.Identifier('genres'),
+                                                     field=sql.Identifier('genre')), (data['genre'],))
+            id_genres = self.cur.fetchone()[0]
+            # Заполнение таблицы types и получение id_types
+            self.cur.execute("""
+                    INSERT INTO types (type, season) 
+                    VALUES (LOWER(%(type)s), LOWER(%(season)s)) 
+                    ON CONFLICT (season) DO UPDATE SET season=LOWER(Excluded.season)
+                    Returning id;
+                    """, data)
+            id_types = self.cur.fetchone()[0]
+            # # Заполнение таблицы films и получение id_films
+            self.cur.execute("""
+                                INSERT INTO films (name, id_types, id_genres, id_years, time, kinopoisk_id, 
+                                kinopoisk, imdb) 
+                                VALUES (%(name)s, %(id_types)s, %(id_genres)s, %(id_years)s, %(time)s, %(kinopoisk_id)s,
+                                %(kinopoisk)s, %(imdb)s) 
+                                Returning id;
+                                """, {'name': data['name'], 'id_types': id_types, 'id_genres': id_genres,
+                                      'id_years': id_years, 'time': data['time'], 'kinopoisk_id': data['id_kinopoisk'],
+                                      'kinopoisk': data['kinopoisk'], 'imdb': data['imdb']})
+            id_films = self.cur.fetchone()[0]
         except Exception as err:
             print('Error! ', err)
             self.conn.rollback()
         else:
             print('Запрос выполнен. Делается коммит.')
-            print(f'ID = {self.cur.fetchone()[0]}')
+            print(f'ID years = {id_years}')
+            print(f'ID genres = {id_genres}')
+            print(f'ID types = {id_types}')
+            print(f'ID films = {id_films}')
             self.conn.commit()
         # print(self.cur.fetchall())
 
