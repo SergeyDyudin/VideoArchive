a732b36d8158fc1ff806c35d2b835c78abd5dc2c
Sergey Dyudin <sergeydyudin@yandex.ru>
2021-01-13 22:16:23 +0300

    Update description script

--- Scripts/add_description_in_xlsx.py
+++ Scripts/add_description_in_xlsx.py
@@ -1,13 +1,17 @@
 import openpyxl
 from openpyxl.styles import Font
 import time
-import re
 from kinopoisk_parser import KinopoiskParser
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from pathlib import Path
 
 browser = KinopoiskParser()
-browser.open_selenium()
-wb = openpyxl.load_workbook(filename='C:/install/Films.xlsx')
+driver_path = str(Path(__file__).parent.parent.joinpath('web-drivers').joinpath('chromedriver'))
+file_xlsx = Path(__file__).parent.parent.joinpath('database').joinpath('Films.xlsx')
+
+browser.browser = webdriver.Chrome(driver_path)
+wb = openpyxl.load_workbook(filename=file_xlsx)
 ws = wb.active
 font = Font(name='Times New Roman',
                     size=12,
@@ -17,33 +21,26 @@ font = Font(name='Times New Roman',
                     underline='none',
                     strike=False,
                     color='FF000000')
-for row in ws.iter_rows(min_row=2235, max_row=ws.max_row):
+for row in ws.iter_rows(min_row=2, max_row=4):
+# for row in ws.iter_rows(min_row=2, max_row=ws.max_row):
     result = {}
-    if row[1].value == 'Фильм': continue
     id_film = str(row[5].value)
-    season = int(re.split(' ', row[2].value)[1])
     url = f'https://www.kinopoisk.ru/film/{id_film}'
     browser.browser.get(url)
     time.sleep(5)
     try:
-        element = browser.browser.find_element_by_class_name('table-col-years__seasons')
-        browser.browser.execute_script("arguments[0].click();", element)
-        time.sleep(5)
         get = browser.browser.page_source
         soup = BeautifulSoup(get, features="html.parser")
-        years = []
-        items = soup.find('div', {'class', 'shadow'}).find_all('a', {'class', 'all'})
+        description = ''
+        items = soup.find('div', {'class', 'styles_filmSynopsis__zLClu'}).find_all('p')
         for i in items:
-            if '#y' in i['href']:
-                years += i.contents
-        result["year"] = years
-        result["year"] = result["year"][season - 1]
-        row[4].value = result["year"]
-        row[4].font = font
-        wb.save('C:/install/Films.xlsx')
-        print(f'Даты для {row[0].value} записаны')
+            description += i.contents[0]
+        row[13].value = description
+        row[13].font = font
+        wb.save(file_xlsx)
+        print(f'Описание для {row[0].value} записаны')
     except Exception as exc:
         print(exc)
-        print(f'Даты сезонов не получилось достать. {row[0].value}')
+        print(f'Описание не получилось достать. {row[0].value}')
 wb.close()
 browser.close_selenium()
