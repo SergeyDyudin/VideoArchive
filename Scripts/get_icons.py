057f1def6987a9d4f001df584819cd35762c510b
sergeydyudin <sergeydyudin@yandex.ru>
2021-01-15 20:42:44 +0300

    Update script

--- Scripts/get_icons.py
+++ Scripts/get_icons.py
@@ -1,20 +1,28 @@
 import requests
 import openpyxl
 from pathlib import Path
+import os
 
+
+if os.path.exists('/home/zs-content-02-usr/projects/django/films_site/main/static/main/img/icons'):  # Ubuntu-serv
+    PATH_TO_ICONS = '/home/zs-content-02-usr/projects/django/films_site/main/static/main/img/icons'
+elif os.path.exists('/Users/sergeydyudin/Documents/PycharmProjects/films_site/main/static/main/img/icons'):
+    PATH_TO_ICONS = '/Users/sergeydyudin/Documents/PycharmProjects/films_site/main/static/main/img/icons'
+else:
+    raise 'Добавьте путь к папке иконками на этом PC'
 def get_film_icon():
     file_xlsx = Path(__file__).parent.parent.joinpath('database').joinpath('Films.xlsx')
 
     wb = openpyxl.load_workbook(filename=file_xlsx)
     ws = wb.active
     for row in ws.iter_rows(min_row=2, max_row=ws.max_row):
+        id_xlsx = str(row[0].value)
         id_film = str(row[5].value)
         url = f'https://www.kinopoisk.ru/images/sm_film/{id_film}.jpg'
         req = requests.get(url)
-        with open(f'/Users/sergeydyudin/Documents/PycharmProjects/films_site/main/static/main/img/icons/{id_film}_icon.jpg', 
-            'wb') as icon_file:
+        with open(PATH_TO_ICONS + f'/{id_film}_icon.jpg', 'wb') as icon_file:
             icon_file.write(req.content)
-            print(f'Загружена иконка для фильма {id_film}')
+            print(f'Загружена иконка для фильма {id_film} {id_xlsx}')
     wb.close()
 
 if __name__ == "__main__":
